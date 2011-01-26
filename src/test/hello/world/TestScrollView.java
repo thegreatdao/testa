536bae22195a6609f832064512566a5548c69c89
HL <anotherdayinpara@yahoo.com>
2011-01-25 20:03:53 -0500

    added more viewlayout

---
+++ src/test/hello/world/TestScrollView.java
@@ -0,0 +1,20 @@
+package test.hello.world;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class TestScrollView extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.scroll_view);
+		TextView textView = (TextView) findViewById(R.id.txt);
+		String text = getString(R.string.scroll_text);
+		textView.setText(text);
+	}
+	
+}
