47ab54130ae25f7aa88505b17184b441dd67951f
HL <anotherdayinpara@yahoo.com>
2011-01-27 23:37:16 -0500

    updated doc

--- src/test/hello/world/color/TestColorStateList.java
+++ src/test/hello/world/color/TestColorStateList.java
@@ -3,6 +3,7 @@ package test.hello.world.color;
 import test.hello.world.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class TestColorStateList extends Activity
 {
@@ -12,6 +13,8 @@ public class TestColorStateList extends Activity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.color_state_list);
+		Button button = (Button) findViewById(R.id.Button01);
+		button.setBackgroundResource(R.drawable.insets);
 	}
 	
 }
