2b0fed35293f9c386b77459fed552e85b26166c2
HL <anotherdayinpara@yahoo.com>
2011-02-02 19:56:32 -0500

    finished db

--- src/test/hello/world/db/TestDatabaseActivity.java
+++ src/test/hello/world/db/TestDatabaseActivity.java
@@ -1,13 +1,164 @@
 package test.hello.world.db;
 
-import android.app.Activity;
+import static test.hello.world.db.TestDatabaseHelperAdapter.CONTACT_ADDRESS;
+import static test.hello.world.db.TestDatabaseHelperAdapter.CONTACT_NAME;
+import test.hello.world.R;
+import test.hello.world.db.TestDatabaseHelperAdapter.Contact;
+import android.app.AlertDialog;
+import android.app.ListActivity;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.SimpleCursorAdapter;
 
-public class TestDatabaseActivity extends Activity
+public class TestDatabaseActivity extends ListActivity
 {
+	private Cursor cursor;
+	private TestDatabaseHelperAdapter testDatabaseHelperAdapter;
+	private static final int FIRST = Menu.NONE + 1;
+	private static final int SECOND = FIRST + 1;
+	private ListAdapter listAdapter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
+		testDatabaseHelperAdapter = new TestDatabaseHelperAdapter(this);
+		cursor = testDatabaseHelperAdapter.selectAllContancts();
+		String[] from = new String[]{CONTACT_NAME, CONTACT_ADDRESS};
+		int[] to = new int[]{R.id.name, R.id.address};
+		listAdapter = new SimpleCursorAdapter(this, R.layout.contact, cursor, from, to);
+		setListAdapter(listAdapter);
+		registerForContextMenu(getListView());
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		menu.add(Menu.NONE, Menu.NONE, Menu.NONE, "Add Contact");
+		return(super.onCreateOptionsMenu(menu));
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		switch (item.getItemId())
+		{
+			case Menu.NONE:
+				showContactForm(null, null, -1);
+				return Boolean.TRUE;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
+	
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo)
+	{
+		menu.add(Menu.NONE, FIRST, Menu.NONE, R.string.edit);
+		menu.add(Menu.NONE, SECOND, Menu.NONE, R.string.delete);
+	}
+
+	@Override
+	public boolean onContextItemSelected(MenuItem item)
+	{
+		AdapterView.AdapterContextMenuInfo menuInfo = (AdapterView.AdapterContextMenuInfo)item.getMenuInfo();
+		long id = menuInfo.id;
+		switch (item.getItemId())
+		{
+			case FIRST:
+				Contact contact = testDatabaseHelperAdapter.selectOneById(id);
+				showContactForm(contact.getName(), contact.getAddress(), id);
+				return Boolean.TRUE;
+			case SECOND:
+				confirmDeletion(id);
+				return Boolean.TRUE;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
+	
+	private void confirmDeletion(final long id)
+	{
+		new AlertDialog.Builder(this).setTitle("Are you sure you want to delete this contact?")
+		.setPositiveButton(R.string.delete, new OnClickListener()
+		{
+			@Override
+			public void onClick(DialogInterface arg0, int arg1)
+			{
+				testDatabaseHelperAdapter.deleteContact(id);
+				cursor.requery();
+			}
+		}).setNegativeButton(R.string.cancel, new OnClickListener()
+		{
+			@Override
+			public void onClick(DialogInterface arg0, int arg1)
+			{
+			}
+		}).show();
+	}
+
+	private void showContactForm(String name, String address, final long id)
+	{
+		final View contactForm = initContactFormView(name, address);
+		
+		new AlertDialog.Builder(this).setTitle("Contact Form")
+		.setView(contactForm).setPositiveButton(R.string.addOrEdit, new OnClickListener()
+		{
+			
+			@Override
+			public void onClick(DialogInterface arg0, int arg1)
+			{
+				String name = ((EditText)contactForm.findViewById(R.id.name)).getText().toString();
+				String address = ((EditText)contactForm.findViewById(R.id.address)).getText().toString();
+				saveContact(name, address, id);
+				cursor.requery();
+			}
+		}).setNegativeButton(R.string.cancel, new OnClickListener()
+		{
+			
+			@Override
+			public void onClick(DialogInterface arg0, int arg1)
+			{
+			}
+		}).show();
+	}
+
+	private View initContactFormView(String name, String address)
+	{
+		LayoutInflater layoutInflator = LayoutInflater.from(this);
+		View contactForm = layoutInflator.inflate(R.layout.contact_form, null);
+		EditText nameEditText = (EditText)contactForm.findViewById(R.id.name);
+		EditText addressEditText = (EditText)contactForm.findViewById(R.id.address);
+		if(name != null && name.trim().length()!=0)
+		{
+			nameEditText.setText(name);
+		}
+		if(address != null && address.trim().length()!=0)
+		{
+			addressEditText.setText(address);
+		}
+		return contactForm;
+	}
+	
+	private void saveContact(String name, String address, long id)
+	{
+		if(id == -1)
+		{
+			testDatabaseHelperAdapter.createContact(name, address);
+		}
+		else
+		{
+			testDatabaseHelperAdapter.updateContact(id, name, address);
+		}
 	}
 }
--- src/test/hello/world/db/TestDatabaseHelperAdapter.java
+++ src/test/hello/world/db/TestDatabaseHelperAdapter.java
@@ -2,6 +2,7 @@ package test.hello.world.db;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
@@ -9,10 +10,11 @@ import android.database.sqlite.SQLiteDatabase.CursorFactory;
 public class TestDatabaseHelperAdapter
 {
 	private static final int DATABASE_VERSION = 1;
+	private static final String DATABASE_NAME = "test.db";
 	private static final String CONTACT_TABLE = "contact";
 	private static final String PRIMARY_KEY = "_id";
-	private static final String CONTACT_NAME = "name";
-	private static final String CONTACT_ADDRESS = "address";
+	public static final String CONTACT_NAME = "name";
+	public static final String CONTACT_ADDRESS = "address";
 	private static final String CREATE_CONTACT_TABLE = "CREATE TABLE " + CONTACT_TABLE + " ("
 													+ PRIMARY_KEY + " INTEGER PRIMARY KEY AUTOINCREMENT, "
 													+ CONTACT_NAME + " TEXT NOT NULL, " + CONTACT_ADDRESS
@@ -22,7 +24,7 @@ public class TestDatabaseHelperAdapter
 	
 	public TestDatabaseHelperAdapter(Context context)
 	{
-		testDatabaseHelper = new TestDatabaseHelper(context, CONTACT_TABLE, null, DATABASE_VERSION);
+		testDatabaseHelper = new TestDatabaseHelper(context, DATABASE_NAME, null, DATABASE_VERSION);
 		sQLiteDatabase = testDatabaseHelper.getWritableDatabase();
 	}
 	
@@ -55,12 +57,29 @@ public class TestDatabaseHelperAdapter
 		return sQLiteDatabase.update(CONTACT_TABLE, contentValues, PRIMARY_KEY + "=" + id, null) > 0;
 	}
 	
+	public Cursor selectAllContancts()
+	{
+		String[] columns = {CONTACT_NAME, CONTACT_ADDRESS, PRIMARY_KEY};
+		return sQLiteDatabase.query(CONTACT_TABLE, columns, null, null, null, null, null);
+	}
+	
+	public Contact selectOneById(long id)
+	{
+		Contact contact = new Contact();
+		Cursor cursor = sQLiteDatabase.query(CONTACT_TABLE, new String[]{CONTACT_NAME, CONTACT_ADDRESS}, PRIMARY_KEY + "=" + id, null, null, null, null);
+		if(cursor.getCount() != 0)
+		{
+			cursor.moveToFirst();
+			contact.setName(cursor.getString(0));
+			contact.setAddress(cursor.getString(1));
+		}
+		return contact;
+	}
 	/*
 	 * use this class life cycle methods to manage database updates
 	 */
 	private static class TestDatabaseHelper extends SQLiteOpenHelper
 	{
-
 		public TestDatabaseHelper(Context context, String name, CursorFactory factory, int version)
 		{
 			super(context, name, factory, version);
@@ -80,4 +99,33 @@ public class TestDatabaseHelperAdapter
 		
 	}
 	
+	/*
+	 * you can use public fields instead of private onew with getters and setters
+	 * to speed up a bit for Android applicatoins
+	 */
+	public static class Contact
+	{
+		private String name;
+		private String address;
+		
+		public String getName()
+		{
+			return name;
+		}
+		
+		public void setName(String name)
+		{
+			this.name = name;
+		}
+		
+		public String getAddress()
+		{
+			return address;
+		}
+		
+		public void setAddress(String address)
+		{
+			this.address = address;
+		}
+	}
 }
